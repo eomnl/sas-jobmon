b175921f25f875b58fbbed60447ea66cff00cac1
bheinsius <bheinsius@eom.nl>
2016-12-31 22:56:32 +0100

    SASBatchServer: New linux scripts
    - moved settings/options to dimon_usermods.sh
    - honor SAS logfile names, with options
    - debug env vars to /tmp/dimon-debug-${USER}.txt

--- SASBatch/BatchServer/Linux/dimon_pre.sh
+++ SASBatch/BatchServer/Linux/dimon_pre.sh
@@ -1,63 +1,139 @@
 #!/bin/ksh -p
-# -----------------------------------------------------------------
+# ---------------------------------------------------------------------
 # program : dimon_pre.sh
 # version : 3.1
 # date    : 27nov16
-# purpose : execute dimonStartJob.sas
+# purpose : execute dimon_job_start.sas
 #
 # Uncomment the set -x to run in debug mode
 # set -x
 #
+# Do NOT modify this file.  Any additions or changes should be made in
+# dimon_usermods.sh.
+#
 # change history
 # date     by      changes
-# -------  ------  ------------------------------------------------
+# -------  ------  ----------------------------------------------------
 # 29nov12  eombah  use less envvars
 #                  added comments
 # 27nov16  eombah  Updated for v3
-# -----------------------------------------------------------------
+# ---------------------------------------------------------------------
 
-DATETIME="`date '+%Y.%m.%d_%H.%M.%S'`"
-SASENV=$APPSERVER_ROOT
-SASCMD=$SAS_COMMAND
-SASCFG=$SASENV/BatchServer/sasv9.cfg
-SASLOGDIR=$SASENV/BatchServer/Logs
-DIMONLOGDIR=$SASLOGDIR
-DIMONSCRIPTDIR=$SASENV/SASEnvironment/SASCode/dimon
-SASAUTOEXEC=$SASENV/BatchServer/autoexec.sas
-CONTINUEONDIMONFAIL=YES
-DIMONDEBUG=NO
+# only execute this code when LSF variables are set
+if [[ ! -z "$LSB_JOBID" && ! -z "$LSB_JOBNAME" ]] ; then
 
-# Set SASLOGFILE and SASLSTFILE
-# Strip : . \ from LSB_JOBNAME
-FILENAME_PREFIX=${LSB_JOBID}_$(echo "$LSB_JOBNAME" | sed 's/:/_/g' | sed 's/\\/_/g' | sed 's/\./_/g' | sed 's/__/_/g')
-SASLOGFILE=$SASLOGDIR/${FILENAME_PREFIX}_${DATETIME}.log
-SASLSTFILE=$SASLOGDIR/${FILENAME_PREFIX}_${DATETIME}.lst
+  # Do NOT modify this file.  Any additions or changes should be made in
+  # dimon_usermods.sh.
 
-dir=$(pwd)
-cd $CONFIGDIR
+  # set default options
+  DIMON_SASLOGFILE_RESOLVE_YMDHMS=YES
+  DIMON_SASLOGFILE_PREPEND_JOBID_FLOWID_USER=NO
+  DIMON_SASLOGFILE_APPEND_DATETIME=NO
+  DIMON_CONTINUEONFAIL=YES
+  DIMON_DEBUG=NO
 
-if [ "$DIMONDEBUG" -eq "YES" ] ; then
-  set > /tmp/dimon-debug.txt
-fi
-
-# -----------------------------------------------------------------
-# Execute dimonStartJob.sas
-# -----------------------------------------------------------------
-DIMONLOGFILE=${DIMONLOGDIR}/${FILENAME_PREFIX}_${DATETIME}_dimonStartJob.log
-"$SASCMD" -lrecl 32767 -sysin "$DIMONSCRIPTDIR/dimonStartJob.sas" -autoexec "$SASAUTOEXEC" -log "$DIMONLOGFILE" -set LSB_JOBNAME "$LSB_JOBNAME" -set SASLOGFILE "$SASLOGFILE" -set SASLSTFILE "$SASLSTFILE" -set LSB_JOBID "$LSB_JOBID" 
-rc_dimon=$?
-if [ "$rc_dimon" -gt "1" ] ; then
-  echo ERROR: Could not register the Start of the Job. RC=$rc_dimon
-  echo ERROR: See log file $DIMONLOGFILE
-  if [ "$CONTINUEONDIMONFAIL" != "YES" ] ; then
-    cd $dir
-    exit "$rc_dimon"
+  # overwrite default options with options from dimon_usermods.sh
+  DIMON_USERMODS=$APPSERVER_ROOT/BatchServer/dimon_usermods.sh
+  if [[ -e $DIMON_USERMODS ]] ; then
+    . $DIMON_USERMODS
   fi
-else
-  if [ "$DIMONDEBUG" != "YES" ] ; then
-    rm "$DIMONLOGFILE"
+
+  DIMON_DATETIME="$(date '+%Y.%m.%d_%H.%M.%S')"
+
+  if [ "$DIMON_SASLOGFILE_RESOLVE_YMDHMS" = "YES" ] ; then
+    # resolve #Y.#m.#d_#H.#M.#s in the  command line arguments to a real datetime
+    DIMON_CMDLINEARGS=$(echo "$@" | sed "s/#Y.#m.#d_#H.#M.#s/${DIMON_DATETIME}/g")
   fi
+
+  # get LSF FLOWID and JOBID
+  DIMON_LSF_FLOWID=$(echo "$LSB_JOBNAME" | awk -F':' '{print $1}')
+  DIMON_LSF_JOBID=${LSB_JOBID}
+
+  # get -log parm from command line
+  for parm in $DIMON_CMDLINEARGS; do
+    ((index=index+1))
+    if [ "$parm" = "-log" ] ; then
+      logparmpos=$index
+    fi
+    # get logfile right after -log parm
+    if [ "$index" = $((logparmpos+1)) ] ; then
+      DIMON_SASLOGFILE=$parm
+      # No reason to continue this loop
+      break
+    fi
+  done
+
+  # get -print parm from command line
+  for parm in $DIMON_CMDLINEARGS; do
+    ((index=index+1))
+    if [ "$parm" = "-print" ] ; then
+      printparmpos=$index
+    fi
+    # get lstfile right after -lst parm
+    if [ "$index" = $((printparmpos+1)) ] ; then
+      DIMON_SASLSTFILE=$parm
+      # No reason to continue this loop
+      break
+    fi
+  done
+
+  # save for replace using sed later on
+  DIMON_ORIGINAL_SASLOGFILE=$DIMON_SASLOGFILE
+
+  DIMON_LOGDIR=$(dirname $DIMON_SASLOGFILE)
+  DIMON_SCRIPTDIR=$CONFIGDIR/../SASEnvironment/SASCode/dimon
+
+  # LSB_JOBNAME contains something like 59:lsfadmin:testflow21:testflow2:testjob2;
+  # Replace : . \ by _ in LSB_JOBNAME
+  DIMON_JOBNAME=$(echo "$LSB_JOBNAME" | sed 's/:/_/g' | sed 's/\\/_/g' | sed 's/\./_/g' | sed 's/__/_/g')
+
+  if [ "$DIMON_SASLOGFILE_PREPEND_JOBID_FLOWID_USER" = "YES" ] ; then
+    # prefix logfile name with jobid, flowid, and user
+    DIMON_SASLOGFILE=${DIMON_LOGDIR}/${DIMON_LSF_JOBID}_${DIMON_LSF_FLOWID}_${USER}_$(basename ${DIMON_ORIGINAL_SASLOGFILE})
+  fi
+
+  # if logfile name ends with an underscore, append datetime to it
+  if [ "$DIMON_SASLOGFILE_APPEND_DATETIME" = "YES" ] ; then
+    if [[ "$DIMON_SASLOGFILE" = *_ ]] ; then
+      # append {DIMON_DATETIME}.log to ${DIMON_SASLOGFILE}
+      DIMON_SASLOGFILE=${DIMON_SASLOGFILE}${DIMON_DATETIME}.log
+    fi
+  fi
+
+  # replace original sas logfile name in command line args with new composed name
+  DIMON_CMDLINEARGS=$(echo "$DIMON_CMDLINEARGS" | sed "s:${DIMON_ORIGINAL_SASLOGFILE}:${DIMON_SASLOGFILE}:g")
+
+  if [ ! -z "$DIMON_SASLOGFILE" ] ; then
+    DIMON_SETSASLOGFILE="-set SASLOGFILE \"${DIMON_SASLOGFILE}\""
+  fi
+  if [ ! -z "$DIMON_SASLSTFILE" ] ; then
+    DIMON_SETSASLSTFILE="-set SASLSTFILE \"${DIMON_SASLSTFILE}\""
+  fi
+
+  DIMON_LOGFILE=$(echo "$DIMON_SASLOGFILE" | sed "s:\.log$:_dimon_job_start.log:g")
+
+  if [ "$DIMON_DEBUG" = "YES" ] ; then
+    # output system variables to debug file
+    set > /tmp/dimon-debug-${USER}.txt
+  fi
+
+  # -----------------------------------------------------------------
+  # Execute dimon_job_start.sas
+  # -----------------------------------------------------------------
+  "$SAS_COMMAND" -sysin "${DIMON_SCRIPTDIR}/dimon_job_start.sas" -log "${DIMON_LOGFILE}" -set LSB_JOBNAME "${LSB_JOBNAME}" ${DIMON_SETSASLOGFILE} ${DIMON_SETSASLSTFILE} -set LSB_JOBID "${LSB_JOBID}"
+  DIMON_RC=$?
+  if [ $DIMON_RC -gt 1 ] ; then
+    echo ERROR: Registering DIMON Job Start event failed for job \"${LSB_JOBNAME}\". RC=$DIMON_RC
+    echo ERROR: See logfile $DIMON_LOGFILE
+    if [ "$DIMON_CONTINUEONFAIL" != "YES" ] ; then
+      # exit sasbatch.sh with non-zero return code if continue on fail = NO
+      exit "$DIMON_RC"
+    fi
+  else
+    if [ "$DIMON_DEBUG" != "YES" ] ; then
+      # remove dimon_job_start logfile if we are not in debug mode
+      rm "$DIMON_LOGFILE"
+    fi
+  fi
+
 fi
-
-cd $dir
-
