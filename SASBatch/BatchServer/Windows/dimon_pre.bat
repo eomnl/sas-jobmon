6a7f53ddbfedda6cc15df86982a490bf08117c01
bheinsius <bheinsius@eom.nl>
2017-01-29 23:54:32 +0100

    New versions of dimon batch scripts

--- SASBatch/BatchServer/Windows/dimon_pre.bat
+++ SASBatch/BatchServer/Windows/dimon_pre.bat
@@ -1,6 +1,3 @@
-set LSB_JOBNAME=59:lsfadmin:testflow21:testflow2:testjob2
-set LSB_JOBID=123
-@echo off
 REM -----------------------------------------------------------------
 REM program : dimon_pre.bat
 REM version : 3.1
@@ -14,7 +11,23 @@ REM 30nov12  eombah  initial version
 REM 27nov16  eombah  updated for v3
 REM -----------------------------------------------------------------
 
-SET DIMON_CMDLINEARGS=
+REM set default options
+SET DIMON_SASLOGFILE_RESOLVE_YMDHMS=YES
+SET DIMON_SASLOGFILE_PREPEND_JOBID_FLOWID_USER=YES
+SET DIMON_SASLOGFILE_APPEND_DATETIME=YES
+SET DIMON_CONTINUEONFAIL=YES
+SET DIMON_DEBUG=NO
+
+REM overwrite default options with options from dimon_usermods.sh (if it exists)
+SET DIMON_USERMODS=%APPSERVER_ROOT%\BatchServer\dimon_usermods.bat
+IF EXIST "%DIMON_USERMODS%" (
+  CALL "%DIMON_USERMODS%"
+) else (
+  ECHO NOTE: %DIMON_USERMODS% was not executed because it does not exist.
+)
+
+SET DIMON_SCRIPTDIR=%CONFIGDIR%/../SASEnvironment/SASCode/dimon
+
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM only execute this code when LSF variables are set
@@ -24,26 +37,19 @@ IF DEFINED LSB_JOBID (
     REM Do NOT modify this file.  Any additions or changes should be made in
     REM dimon_usermods.bat.
 
-    REM set default options
-    SET DIMON_SASLOGFILE_RESOLVE_YMDHMS=YES
-    SET DIMON_SASLOGFILE_PREPEND_JOBID_FLOWID_USER=NO
-    SET DIMON_SASLOGFILE_APPEND_DATETIME=NO
-    SET DIMON_CONTINUEONFAIL=YES
-    SET DIMON_DEBUG=NO
+    REM get LSF FLOWID
+    FOR /f "tokens=1 delims=:" %%a IN ("%LSB_JOBNAME%") DO SET DIMON_LSF_FLOWID=%%a
 
-    REM overwrite default options with options from dimon_usermods.sh
-    SET DIMON_USERMODS=%APPSERVER_ROOT%\BatchServer\dimon_usermods.bat
-    IF EXIST "!DIMON_USERMODS!" (
-      CALL "!DIMON_USERMODS!"
-    ) else (
-      ECHO NOTE: !DIMON_USERMODS! was not executed because it does not exist.
-    )
-
-    REM Set DIMON_CMDLINEARGS as a copy of all command line parameters
-    SET DIMON_CMDLINEARGS=%*
+    REM get LSF JOBID
+    SET DIMON_LSF_JOBID=%LSB_JOBID%
 
     REM Get current date/time (locale-independent from PowerShell)
     FOR /F "delims=" %%# in ('powershell get-date -format "{yyyyMMdd_HHmmss}"') do @SET DIMON_DATETIME=%%#
+
+    SET DIMON_SASLOGFILE=
+
+    REM Set DIMON_CMDLINEARGS as a copy of all command line parameters
+    SET DIMON_CMDLINEARGS_ORG=%DIMON_CMDLINEARGS%
 
     IF "!DIMON_CMDLINEARGS!" NEQ "" (
       IF "!DIMON_SASLOGFILE_RESOLVE_YMDHMS!" == "YES" (
@@ -52,17 +58,11 @@ IF DEFINED LSB_JOBID (
       )
     )
 
-    REM get LSF FLOWID
-    FOR /f "tokens=1 delims=:" %%a IN ("%LSB_JOBNAME%") DO SET DIMON_LSF_FLOWID=%%a
-
-    REM get LSF JOBID
-    SET DIMON_LSF_JOBID=%LSB_JOBID%
-
     REM get -log parm from DIMON_CMDLINEARGS
     SET logparmindex=0
     SET DIMON_SASLOGFILE=
     SET /a index=0
-    for %%a in (!dimon_cmdlineargs!) DO (
+    for %%a in (!DIMON_CMDLINEARGS!) DO (
       SET /a index+=1
       if "%%a" EQU "-log" (
         SET /a logparmindex=index+1
@@ -95,9 +95,6 @@ IF DEFINED LSB_JOBID (
       SET DIMON_LOGFILE=%%~nxA
     )
 
-    REM set directory containing DIMon SAS programs
-    SET DIMON_SCRIPTDIR=%CONFIGDIR%/../SASEnvironment/SASCode/dimon
-
     REM LSB_JOBNAME contains something like 59:lsfadmin:testflow21:testflow2:testjob2
     REM Replace : . \ by _ in LSB_JOBNAME
     SET DIMON_JOBNAME=!LSB_JOBNAME::=_!
@@ -121,36 +118,36 @@ IF DEFINED LSB_JOBID (
       CALL SET DIMON_CMDLINEARGS=%%DIMON_CMDLINEARGS:!DIMON_ORIGINAL_SASLOGFILE!=!DIMON_SASLOGFILE!%%
     )
 
-    IF DEFINED DIMON_SASLOGFILE SET DIMON_SETSASLOGFILE="-set SASLOGFILE ""!DIMON_SASLOGFILE!"""
-    IF DEFINED DIMON_SASLSTFILE SET DIMON_SETSASLSTFILE="-set SASLSTFILE ""!DIMON_SASLSTFILE!"""
+    IF DEFINED DIMON_SASLOGFILE SET DIMON_SETSASLOGFILE=-set SASLOGFILE "!DIMON_SASLOGFILE!"
+    IF DEFINED DIMON_SASLSTFILE SET DIMON_SETSASLSTFILE=-set SASLSTFILE "!DIMON_SASLSTFILE!"
 
     SET DIMON_JOB_START_LOG=!DIMON_SASLOGFILE:.log=_dimon_job_start.log!
 
-    if "!DIMON_DEBUG!" == "YES" (
+    IF "%DIMON_DEBUG%" EQU "YES" (
       REM output system variables to debug file
-      set > %TEMP%\dimon-debug-%USERNAME%.txt
+      SET > "%TEMP%\dimon-debug-%USERNAME%.txt"
     )
 
     REM -----------------------------------------------------------------
     REM Execute dimon_job_start.sas
     REM -----------------------------------------------------------------
-    "%SAS_COMMAND%" -sysin "!DIMON_SCRIPTDIR!/dimon_job_start.sas" -log "!DIMON_JOB_START_LOG!" -set LSB_JOBNAME "!LSB_JOBNAME!" !DIMON_SETSASLOGFILE! !DIMON_SETSASLSTFILE! -set LSB_JOBID "!DIMON_LSF_JOBID!"
+    "%SAS_COMMAND%" %CMD_OPTIONS% -sysin "%DIMON_SCRIPTDIR%/dimon_job_start.sas" -log "!DIMON_JOB_START_LOG!" -set LSB_JOBNAME "!LSB_JOBNAME!" !DIMON_SETSASLOGFILE! !DIMON_SETSASLSTFILE! -set LSB_JOBID "!DIMON_LSF_JOBID!"
     SET DIMON_RC=%ERRORLEVEL%
     IF !DIMON_RC! GTR 1 (
-      ECHO ERROR: Registering DIMON Job Start event failed for job "!DIMON_LSF_JOBNAME!". RC=!DIMON_RC!
-      ECHO ERROR: See log file %DIMON_LOGFILE%
-      IF "!DIMON_CONTINUEONFAIL!" NEQ "YES" (
+      ECHO ERROR: Registering DIMON Job Start event failed for job "%LSB_JOBNAME%". RC=!DIMON_RC!
+      ECHO ERROR: See log file !DIMON_JOB_START_LOG!
+      IF "%DIMON_CONTINUEONFAIL%" NEQ "YES" (
         REM exit sasbatch.sh with non-zero return code if continue on fail = NO
         EXIT /b !DIMON_RC!
-      ) ELSE (
-        IF "!DIMON_DEBUG!" NEQ "YES" (
-          REM remove dimon_job_start logfile if we are not in debug mode
-          DEL "!DIMON_JOB_START_LOG!"
-        )
+      )
+    ) ELSE (
+      IF "!DIMON_DEBUG!" NEQ "YES" (
+        REM remove file DIMON_JOB_START_LOG if we are not in debug mode
+        DEL "!DIMON_JOB_START_LOG!"
       )
     )
 
   )
 )
 
-ENDLOCAL & SET DIMON_CMDLINEARGS=%DIMON_CMDLINEARGS%
+ENDLOCAL & SET DIMON_CMDLINEARGS=%DIMON_CMDLINEARGS%& SET DIMON_SASLOGFILE=%DIMON_SASLOGFILE%
