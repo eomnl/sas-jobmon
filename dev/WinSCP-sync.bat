742abd4a58346c9ca16566cfc2710169157350ea
bheinsius <bheinsius@eom.nl>
2020-05-16 10:48:50 +0200

    removed dev

--- dev/WinSCP-sync.bat
+++
@@ -1,3 +0,0 @@
-start /b "WinSCP Sync Webapp"  "C:\Program Files (x86)\WinSCP\WinSCP.com" /script="C:\Users\bheinsius\Documents\GitHub\eom-sas-dimon\dev\Scripts\winscp-sync-Webapp.txt"
-start /b "WinSCP Sync Webapp"  "C:\Program Files (x86)\WinSCP\WinSCP.com" /script="C:\Users\bheinsius\Documents\GitHub\eom-sas-dimon\dev\Scripts\winscp-sync-SASMacro.txt"
-start /b "WinSCP Sync Webapp"  "C:\Program Files (x86)\WinSCP\WinSCP.com" /script="C:\Users\bheinsius\Documents\GitHub\eom-sas-dimon\dev\Scripts\winscp-sync-BatchServer.txt"
